aa0751023a99f86280168aaff4fbc1fe654500d1
SeokHwan13 <seokhwan1113@naver.com>
2025-12-05 17:02:30 +0900

    [fix] build.sh 수정

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-cd ../
-mkdir output
-cp -R ./Team_5_Front_Repository/* ./output
-cp -R ./output ./Team_5_Front_Repository/
+
+# 현재 repo 루트 기준으로 output 폴더 생성
+mkdir -p output
+
+# Team_5_Front_Repository 안의 파일을 output으로 복사
+cp -R ./Team_5_Front_Repository/* ./output/
